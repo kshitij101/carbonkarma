25bd144bf4bc1c05b167d81807fdc40cb44defdb
Prem Banker <bankerprem3@gmail.com>
2024-04-07 09:23:10 -0400

    changed some shit

--- carbonkarma/src/App.js
+++ carbonkarma/src/App.js
@@ -4,17 +4,24 @@ import Home from './Home';
 import Analytics from './Analytics';
 import Navigation from './Navigation'; // Import Navigation component
 import './App.css';
+import { AnalyticsProvider } from './context/AnalyticsContext';
+
 
 const App = () => {
   return (
     <Router>
       <div className='gradient-background'>
         <div className='container'>
-     
+        <AnalyticsProvider>
           <Routes>
             <Route path="/" element={<Home />} />
+            
             <Route path="/analytics" element={<Analytics />} />
+            
+            
           </Routes>
+
+          </AnalyticsProvider>
         </div>
 
       </div>
---
+++ carbonkarma/src/context/AnalyticsContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from 'react';
+
+// Create the context
+const AnalyticsContext = createContext();
+
+// Create the context provider
+export const AnalyticsProvider = ({ children }) => {
+  // State or functions related to analytics
+  const [analyticsData, setAnalyticsData] = useState({'walking': 1});
+
+  // Define functions to update analytics data    
+  const updateAnalyticsData = (data) => {
+   
+    setAnalyticsData(data);
+  };
+
+  // Provide the context value
+  return (
+    <AnalyticsContext.Provider value={{ analyticsData, updateAnalyticsData }}>
+      {children}
+    </AnalyticsContext.Provider>
+  );
+};
+   
+// Export the context and its consumer
+export default AnalyticsContext;
