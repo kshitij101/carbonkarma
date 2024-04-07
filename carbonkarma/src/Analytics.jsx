412a531068ed52e742166a3d9e8df46d5840a44a
Prem Banker <bankerprem3@gmail.com>
2024-04-07 09:55:44 -0400

    added css analytics changes

--- carbonkarma/src/Analytics.jsx
+++ carbonkarma/src/Analytics.jsx
@@ -3,7 +3,9 @@ import React, { useContext, useEffect } from 'react';
 import { getEmailObjects, getTotalDistanceByTypeAndDate } from './utils/util'; // Import the function
 import DateRangePicker from './Datepicker';
 import { withAuthInfo } from '@propelauth/react'
+import './Analytics.css';
 import AnalyticsContext from './context/AnalyticsContext';
+import hero from './assets/analyticshero.svg'; // Import the SVG file
 
 // import './Analytics.css'; // Assuming you have a CSS file for styling
 
@@ -15,8 +17,17 @@ const Analytics = withAuthInfo((props) => {
         if (travelData != null)
             return Object.values(travelData).reduce((sum, value) => sum + value, 0);
         return 0;
-    
-        }
+
+    }
+
+    function calculateTotalGreenTravel(travelData) {
+        if (travelData != null)
+            return Object.values(travelData).reduce((sum, value) => sum + value, 0);
+        return 0;
+
+    }
+
+
 
     const { analyticsData, updateAnalyticsData } = useContext(AnalyticsContext);
 
@@ -24,9 +35,42 @@ const Analytics = withAuthInfo((props) => {
     return (
         <div>
             <DateRangePicker></DateRangePicker>
-            <h2>Analytics Page</h2>
-            <p>analytics:  {calculateTotalTravel(analyticsData)} </p>
-            {/* Add your analytics page content here */}
+            <div className='analytics-parent'>
+                <div className="first-column">
+                    <div className='title-text'>Welcome Back, {props.user.firstName}</div>
+
+                    calculateTotalGreenTravel
+                    <div className="svg-container">
+                        {/* Replace this with your SVG component */}
+                        <img src={hero} alt="Logo" />
+                    </div>
+
+
+                    <div className='subtitle-text'>You traveled  {calculateTotalTravel(analyticsData)} km in the selected time</div>
+
+
+                    <div className="stat-row">
+                        <div className="stat-column">
+                            <div>Total Travel</div>
+                            <div>{calculateTotalTravel(analyticsData)}</div>
+                        </div>
+                        <div className="stat-column2">
+                            <div>Total Green Travel</div>
+                            <div>{calculateTotalGreenTravel(analyticsData)}</div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="second-column">
+                    <div className='graph-title'> Your Total travel </div>
+
+                </div>
+
+            </div>
+
+
+
+
         </div>
     );
 })
